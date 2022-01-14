378d0dc306e703760bd300c884fca1c9a1fd0756
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-14 15:22:00 +0200

    Добавлены стандарт файлы, было начато первое задание

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryItemEl = ({ preview, original, description } = {}) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+</li>`
+};
+
+const getGalleryItemEl = galleryItems.map(el => {
+    return createGalleryItemEl(el);
+});
+
+galleryEl.insertAdjacentHTML('afterBegin', getGalleryItemEl.join(''));
+
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay:250, captionType: 'attr', captions: true, captionPosition: 'bottom', captionSelector: 'img', captionsData: 'alt' });
+
+galleryEl.addEventListener('click', event => {
+   event.preventDefault();
+   
+    if (event.target.nodeName !== 'IMG') {
+    return;
+    };
+});

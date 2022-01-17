e628566014f45f8a98aec3d7c08e615afe1e754d
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-17 16:21:43 +0200

    Было сделано второе задание, подключены throttle и vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// console.dir(iframe);
+
+const onPlay = function(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if( !localStorage.getItem("videoplayer-current-time")){
+    return;
+};
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+
+
 

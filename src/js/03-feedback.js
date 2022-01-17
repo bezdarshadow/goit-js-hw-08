8e7a4cdf712dd9599c6c04c1946b0bf053eb0897
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-17 20:53:45 +0200

    Был доделан 3 скрипт

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
+import localStorageApi from "./localstorage";
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+const formData = {};
+const onFormInput = event => {
+    const target = event.target;
+    formData[target.name] = target.value;
+    localStorageApi.save('feedback-form-state', formData);
+};
+
+const throttledFunction = throttle(onFormInput, 1000);
+
+feedbackFormEl.addEventListener('input', throttledFunction);
+
+feedbackFormEl.addEventListener('submit', event => {
+    event.preventDefault();
+    event.target.reset();
+    localStorageApi.remove('feedback-form-state');
+    console.log(formData);
+  });
+
+  const fillFormFields = () => {
+    if (!localStorage.getItem('feedback-form-state')) {
+      return;
+    }
+  
+    const localStorageFormData = localStorageApi.load('feedback-form-state');
+    const keys = Object.keys(localStorageFormData);
+  
+    for (const key of keys) {
+        feedbackFormEl.elements[key].value = localStorageFormData[key];
+    }
+  };
+  
+  fillFormFields();
+
+
+
+
+  
 
